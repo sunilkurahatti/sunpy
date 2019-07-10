e3f16531fb9cf6d8e3b1c1aa8a3eae858e3640ef
Sunil Kurahatti <skurahatti@seic.com>
2019-07-10 18:26:18 +0530

    grid search for best params

---
+++ Machine Learning A-Z Template Folder/Part 10 - Model Selection & Boosting/Section 48 - Model Selection/Model_Selection/sunny_grid_search.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 17:35:02 2019
+
+@author: skurahatti
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#importing datasets
+ds=pd.read_csv('Social_Network_Ads.csv')
+X=ds.iloc[:,2:4].values
+y=ds.iloc[:,4].values
+
+#applying feature scaling
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X=sc.fit_transform(X)
+#splitting datasets
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25, random_state=0)
+
+#APPLYING regresseion
+from sklearn.svm import SVC
+classifier=SVC(kernel='rbf',random_state=0)
+classifier.fit(X_train,y_train)
+
+#predicting test sets
+y_pred=classifier.predict(X_test)
+#building confusion matrix
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+
+#k-fold cross validation
+from sklearn.model_selection import cross_val_score
+val_score=cross_val_score(estimator=classifier,X=X_train,y=y_train,n_jobs=-1, cv=20)
+val_score.mean()
+val_score.std()
+
+#Grid search implimentation
+from sklearn.model_selection import GridSearchCV
+param=[{'C':[1,10,100,1000], 'kernel':['linear']},
+        {'C':[36,38,40,42,44,46],'kernel':['rbf'],'gamma':[0.15,0.17,0.19,0.20,0.22,0.24]}]
+gs=GridSearchCV(estimator=classifier,param_grid=param, scoring='accuracy',cv=10)
+gs.fit(X_train,y_train)
+gs.best_score_
+gs.best_estimator_
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
