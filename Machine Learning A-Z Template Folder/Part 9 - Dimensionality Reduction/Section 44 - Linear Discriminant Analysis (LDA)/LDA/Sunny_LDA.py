2f58c34afb390e868a94a225b2e1cfea4b88fc30
Sunil Kurahatti <skurahatti@seic.com>
2019-07-10 14:46:28 +0530

    LDA Dimentionality reduction

---
+++ Machine Learning A-Z Template Folder/Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/LDA/Sunny_LDA.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 13:30:23 2019
+
+@author: skurahatti
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib qt
+
+#importing datasets
+ds=pd.read_csv('Wine.csv')
+X=ds.iloc[:,:13].values
+y=ds.iloc[:,13].values
+
+#applying feaure scaling
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X=sc.fit_transform(X)
+
+#splitting datasets
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+
+#Applying LDA Fimantionality reduction
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+lda=LinearDiscriminantAnalysis()
+X_train=lda.fit_transform(X_train,y_train)
+X_test=lda.transform(X_test)
+a=lda.explained_variance_ratio_
+
+#applying logisitc regression
+from sklearn.linear_model import LogisticRegression
+classifier=LogisticRegression()
+classifier.fit(X_train,y_train)
+
+#predicting test data
+y_pred=classifier.predict(X_test)
+
+#creating confusion matrix
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+cm_train=confusion_matrix(y_train,classifier.predict(X_train))
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
