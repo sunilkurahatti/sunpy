a13306926812721d71e5b228e2aba475e86b4d42
Sunil <sunilkurahatti@gmail.com.com>
2020-01-07 17:46:22 +0530

    2 nd logistic

---
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression/2_sunny_logistic regression.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 19:36:57 2020
+
+@author: sunil
+"""
+import panadas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#importing dadasets
+
