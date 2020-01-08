8081ed87a67e2c35a1fcc80375f29091cde662e0
Sunil <sunilkurahatti@gmail.com.com>
2020-01-08 16:24:56 +0530

    SVC and Kerner SVC 2nd shot

---
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/SVM/2_sunny_SVM_ Regression.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  8 13:39:10 2020
+
+@author: sunil
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#importing datastructure
+ds=pd.read_csv("Social_Network_Ads.csv")
+X=ds.iloc[:,2:4].values
+y=ds.iloc[:,-1].values
+
+#applying statderd scalar
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X=sc_X.fit_transform(X)
+
+#slitting data sets for training
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0,test_size=.20)
+
+#applying regression
+from sklearn.svm import SVC
+classifier=SVC(kernel='linear')
+classifier.fit(X_train,y_train)
+
+#predicting the results
+
+y_pred=classifier.predict(X_test)
+
+#Verify with confusion matrics
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred)
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
