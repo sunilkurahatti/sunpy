64d3b731bc5a209da75a8daf6f0d5573011ba163
Sunil <sunilkurahatti@gmail.com.com>
2021-03-20 19:20:59 +0530

    3 rd slot

---
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 15 - K-Nearest Neighbors (K-NN)/K_Nearest_Neighbors/3_sunny_K-NN_Classifier.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 13:25:04 2021
+
+@author: sunil
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#importing datasets
+ds=pd.read_csv("Social_Network_Ads.csv")
+X=ds.iloc[:,2:-1].values
+y=ds.iloc[:,-1].values
+
+#applying standerd scalar
+from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+X=sc_x.fit_transform(X)
+
+#splitting the datasets
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0,test_size=0.25)
+
+#building the model
+from sklearn.neighbors import KNeighborsClassifier
+clasdifier=KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
+clasdifier.fit(X_train,y_train)
+
+#predicing values
+y_pred=clasdifier.predict(X_test)
+list_pred=np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1)
+
+#confusion matrics
+from sklearn.metrics import confusion_matrix,accuracy_score
+print(accuracy_score(y_test,y_pred))
+cm=confusion_matrix(y_test,y_pred)
+
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clasdifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.50, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clasdifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.50, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
---
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 17 - Kernel SVM/Kernel_SVM/3_sunny_Kerenl_SVM_regression.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 14:34:28 2021
+
+@author: sunil
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#importing datasets
+ds=pd.read_csv("Social_Network_Ads.csv")
+X=ds.iloc[:,2:-1].values
+y=ds.iloc[:,-1].values
+
+#applying standard scalar
+from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+X=sc_x.fit_transform(X)
+
+#splitting data sets
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0,test_size=0.25)
+
+#building svc model
+from sklearn.svm import SVC
+classifier=SVC(kernel='rbf',degree=3)
+classifier.fit(X_train,y_train)
+
+#predictin valyes
+y_pred=classifier.predict(X_test)
+result=np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1)
+
+#confusion matric
+from sklearn.metrics import confusion_matrix, accuracy_score
+print(accuracy_score(y_test,y_pred))
+cm=confusion_matrix(y_test,y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
